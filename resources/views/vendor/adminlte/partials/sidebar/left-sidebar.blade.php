ac1d0e6cfc5f6414468c69d964a8a51834795b52
kandelario <cande.pacheco@gmail.com>
2025-01-22 22:35:07 -0600

    ...

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -21,6 +21,7 @@
                 {{-- Configured sidebar links --}}
                 {{-- @each('adminlte::partials.sidebar.menu-item', $adminlte->menu('sidebar'), 'item') --}}
                 @include('layouts.menu')
+                
             </ul>
         </nav>
     </div>
