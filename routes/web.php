ca86ed19fdb8a07789b5b63bbc8fcbcca39c77fa
kandelario <cande.pacheco@gmail.com>
2025-01-22 22:09:16 -0600

    ..

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ use Illuminate\Http\Request;
 
 
 Route::get('/', function (Request $request) {
-    dd($request);
-    if(!$request->User()) {
+    // dd($request);
+    // if(!$request->User()) {
         return view('auth.login');
-    }else{
-        return redirect ('home');
-    }
+    // }else{
+        // return redirect ('home');
+    // }
 });
 
 // Route::get('/register', function () {
